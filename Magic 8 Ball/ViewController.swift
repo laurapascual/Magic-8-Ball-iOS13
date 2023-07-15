c6b882f8082dc0a8d7521cfb24c932d142a1d569
Laura Pascual <laurapascualmateo@gmail.com>
2023-07-15 16:50:23 +0200

    Add elements

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
+    @IBAction func AskButton(_ sender: UIButton) {
+    }
+    
 
 
 }
